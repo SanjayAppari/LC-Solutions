627948d3eca6b1e57911f7fd357eeed8d96b689e
SanjayAppari <84316748+SanjayAppari@users.noreply.github.com>
2023-06-23 20:51:01 +0530

    Time: 12 ms (46.37%) | Memory: 6.5 MB (74.67%) - LeetSync

--- count-and-say/count-and-say.cpp
+++ count-and-say/count-and-say.cpp
@@ -3,14 +3,12 @@ public:
     string countAndSay(int n) {
         if(n==1) return "1";
         string s=countAndSay(n-1);
-        int i;
         string ans="";
+        int i,c=1;
         for(i=0;i<s.length();i++){
-            int c=1;
-            while(i<s.length()-1 && s[i]==s[i+1]){
-                i++; c++;
-            }
-            ans+=(to_string(c) + s[i]);
+            c=1;
+            while(i<s.length()-1 && s[i]==s[i+1]){ c++;i++;}
+            ans+=to_string(c)+s[i];
         }
         return ans;
     }
