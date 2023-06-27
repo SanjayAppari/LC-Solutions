cddb3a9745b04ef625b1cfc0bb158a85b1c6f1a2
SanjayAppari <84316748+SanjayAppari@users.noreply.github.com>
2023-06-27 12:37:35 +0530

    Time: 873 ms (12.14%) | Memory: 69.4 MB (8.22%) - LeetSync

--- longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -3,17 +3,17 @@ public:
     int longestConsecutive(vector<int>& nums) {
         int i,n=nums.size();
         if(n==0) return 0;
-        sort(nums.begin(),nums.end());
-        int c=1,ans=INT_MIN;
-        for(i=1;i<n;i++){
-            if(nums[i]==nums[i-1]) continue;
-            if(nums[i]==1+nums[i-1]) c++;
+        unordered_map<int,int>m;
+        for(auto i:nums) m[i]++;
+        int ans=0;
+        for(i=0;i<n;i++){
+            if(m[nums[i]-1]!=0) continue;
             else{
-                ans=max(ans,c);
-                c=1;
+                int j=nums[i];
+                while(m[j]!=0) j++;
+                ans=max(ans,j-nums[i]);
             }
-        }
-        ans=max(ans,c);
+        } 
         return ans;
     }
 };
