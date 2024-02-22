cd8ec9988fb03ce6f3af291529be0ce82a71f822
Appari Sai Sanjay <84316748+SanjayAppari@users.noreply.github.com>
2024-02-22 05:46:26 +0530

    Time: 148 ms (45.59%) | Memory: 72.3 MB (28.15%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int>inDegree(n+1),outDegree(n+1);
+        vector<int>in(n+1,0),ot(n+1,0);
         int i;
         for(auto i:trust){
-            inDegree[i[1]]++;
-            outDegree[i[0]]++;
+            in[i[1]]++; ot[i[0]]++;
         }
         for(i=1;i<=n;i++){
-            if(inDegree[i]==n-1 && outDegree[i]==0) return i;
+            if(in[i]==n-1 && ot[i]==0) return i;
         }
         return -1;
     }
