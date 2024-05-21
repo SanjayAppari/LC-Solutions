ee8f598531745f7bee12c523acc6de2ebaaca242
Appari Sai Sanjay <84316748+SanjayAppari@users.noreply.github.com>
2024-05-21 08:25:23 +0530

    Time: 96 ms (17.41%) | Memory: 99.7 MB (5.17%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& a) {
-        int i,n=a.size(),ans=0,m=INT_MAX;
-        for(auto i:a){
-            m=min(m,i);
-            ans=max(ans,abs(m-i));
+        int i,n=a.size(),ans=0;
+        vector<int>suf(n,0);
+        suf[n-1]=a[n-1];
+        for(i=n-2;i>=0;i--){
+            suf[i]=max(a[i],suf[i+1]);
+        }
+        for(i=0;i<n-1;i++){
+            if(a[i]<suf[i+1]) ans=max(ans,suf[i+1]-a[i]);
         }
         return ans;
     }
