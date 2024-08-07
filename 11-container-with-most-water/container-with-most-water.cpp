c18503d3a3043ad7cc5c32227662020b5fc8e0e6
Appari Sai Sanjay <84316748+SanjayAppari@users.noreply.github.com>
2024-08-07 21:22:16 +0530

    Time: 59 ms (69.22%) | Memory: 61.8 MB (14.32%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -4,13 +4,10 @@ public:
         int n=height.size();
         int i=0,j=n-1,ans=0;
         while(i<j){
-            if(height[i]>height[j]){
-                ans=max(ans,(j-i)*height[j]);
-                j--;
-            }
-            else{
-                ans=max(ans,(j-i)*height[i]);
-                i++;
+            if(height[i]<height[j]){
+                ans = max(ans, (j-i)*height[i]); i++;
+            }else{
+                ans = max(ans, (j-i)*height[j]); j--;
             }
         }
         return ans;
