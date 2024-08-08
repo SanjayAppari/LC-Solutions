ce7b7076b6de929c851cdea5289a8487865cebc1
Appari Sai Sanjay <84316748+SanjayAppari@users.noreply.github.com>
2024-08-08 21:42:31 +0530

    Time: 3 ms (39.00%) | Memory: 8.4 MB (8.67%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-    double pow(double a,int b)
-    {
-        // if(a==1) return 1;
+    double pow(double a,int b){
+        if(a==0) return 0;
         if(b==0) return 1;
-        if(b==1) return a;
-        double p=pow(a,b/2);
-        cout<<p<<" ";
-        if(b&1){
-            // cout<<a*p*p<<" ";
-            return a*p*p;
-        }
-        else return p*p;
+        if(b==1 || a==1) return a;
+        double p = pow(a,b/2);
+        if(b&1) return a*p*p;
+        return p*p;
     }
+
     double myPow(double x, int n) {
         if(n<0) return 1/pow(x,n);
         return pow(x,n);
